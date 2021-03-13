ce9538ec7f88fc54edd222c2f02c53a762109485
AdorkablyNicole <hello@nicolegeorge.me>
2021-03-13 16:10:46 +1030

    adding and removing active state border bottom on tab container

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 // selectItem function ~ tab selector
 
 function selectItem(e) {
+  removeBorder();
   // adding border tp ctive tab
   this.classList.add('tab-border');
 }
 
+// removing board from inactive tabs
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove('tab-border'));
+}
 // Event Listener
 tabItems.forEach((item) => item.addEventListener('click', selectItem));
