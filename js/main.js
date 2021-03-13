7b31446db631ba5e332131c701dfaf2d52c47c8a
AdorkablyNicole <hello@nicolegeorge.me>
2021-03-13 16:18:35 +1030

    tab container toggle - stage 3

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ function selectItem(e) {
   removeShow();
   // adding border tp ctive tab
   this.classList.add('tab-border');
+  //   get content items from the DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // add show class to content item
+  tabContentItem.classList.add('show');
 }
 
 // removing board from inactive tabs
